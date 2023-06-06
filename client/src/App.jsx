a56d467844bb7cc276a9ce17d519e6bd3aaa3092
Ihor Onyshchuk <ihor-onyshchuk@MacBook-Pro-Ihor.local>
2023-06-06 17:26:17 +0300

    Add apollo and graphql to the ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Alert } from "shards-react";
+
+import Chat from "./Chat";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <Chat />;
 
 ReactDOM.render(<App />, document.getElementById("app"));
